9b3c1116fbea1f007d4a8f2679a74406a62131bb
GodoyMS <youremail@domain.com>
2023-04-14 17:49:40 -0500

    changed PORT env file

--- src/configs/configEnvs.ts
+++ src/configs/configEnvs.ts
@@ -11,6 +11,7 @@ class Config {
   public SECRET_KEY_TWO: string | undefined;
   public CLIENT_URL: string | undefined;
   public SERVER_PORT: string | undefined;
+  public PORT: string | undefined;
   public REDIS_HOST: string | undefined;
   public CLOUD_NAME: string | undefined;
   public CLOUD_API_KEY: string | undefined;
@@ -34,6 +35,8 @@ class Config {
     this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO;
     this.CLIENT_URL = process.env.CLIENT_URL;
     this.SERVER_PORT = process.env.SERVER_PORT;
+    this.PORT = process.env.PORT;
+
     this.REDIS_HOST = process.env.REDIS_HOST;
     this.CLOUD_NAME = process.env.CLOUD_NAME;
     this.CLOUD_API_KEY = process.env.CLOUD_API_KEY;
